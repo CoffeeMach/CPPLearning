3b5af52b2bf9f65a23416d446482f4afc0e5561c
CoffeeMach <you@example.com>
2026-01-18 17:03:16 +0900

    while statement try

--- prog.cpp
+++ prog.cpp
@@ -2,11 +2,13 @@
 using namespace std;  // "using" declaration is important
 
 int main() {
-    cout << /* "*/" /* "/*" */;  // cool trick
-    
-    cout << "Give me a name and city: " << endl;
-    char name[10] = "", city[10] = "";
-    cin >> name >> city;
-    cout << "The name of the contestant is " << name << " from the land of " << city << "!" << endl;
+    cout << "Enter number: " << endl;
+    int count = 0, sum = 0, val;
+    cin >> val;
+    while (0 <= val) {
+        sum += val;
+        val--;
+    }
+    cout << "The sum is " << sum << endl;
     return 0;
 }
